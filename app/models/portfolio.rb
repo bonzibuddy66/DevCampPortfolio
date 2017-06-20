d282f0be73198b9901344c7eaf47610c456e3478
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-06-20 17:35:39 +0000

    Custom Scopes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,8 @@
 class Portfolio < ApplicationRecord
     validates_presence_of :title , :body, :main_image, :thumb_image
+    
+    def self.programming
+        where(subtitle: 'Programming')
+end
+    scope  :service, -> { where(subtitle: 'My great service')}
 end

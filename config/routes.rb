70602257ff90e6fb04c3dccedda7020a85701487
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-06-20 14:22:33 +0000

    customized portfolio-feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
 
   get 'contact', to: 'pages#contact'
+  
+  get 'portfolio/:id', to: 'portfolios#show' , as: 'portfolio_show'
 
-  resources :portfolios
+  resources :portfolios, except: [:show]
   resources :blogs
   root to: 'pages#home'
 end

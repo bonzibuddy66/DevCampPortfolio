f41282725ed0a35919325341bf810fb86b1daffb
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-06-21 14:26:59 +0000

    Added badges via concern file

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,15 +1,16 @@
 class Portfolio < ApplicationRecord
+    include Placeholder
     validates_presence_of :title , :body, :main_image, :thumb_image
     
     def self.programming
         where(subtitle: 'Programming')
-end
+    end
     scope  :service, -> { where(subtitle: 'My great service')}
     
     after_initialize :set_defaults
     
     def set_defaults
-        self.main_image ||= "http://placehold.it/400x200"
-        self.thumb_image ||= "http://placehold.it/300x500"
+        self.main_image ||=  Placeholder.image_generator(height: '400', width: '200')
+        self.thumb_image ||=  Placeholder.image_generator(height: '300', width: '500')
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620170158) do
+ActiveRecord::Schema.define(version: 20170621140145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170620170158) do
     t.integer "percent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "skillzs", force: :cascade do |t|

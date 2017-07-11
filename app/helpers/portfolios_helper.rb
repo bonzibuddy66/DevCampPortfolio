c865c0d997f0321a16f8d1134beb2d38a64c4c2a
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-11 17:58:46 +0000

    Edited porfolio status

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,15 @@
 module PortfoliosHelper
+  def image_generator(height:, width:)
+    "http://placehold.it/#{height}x#{width}"
+  end
+
+  def portfolio_img img, type
+    if img.model.main_image? || img.model.thumb_image?
+      img
+    elsif type == 'thumb'
+      image_generator(height: '300', width: '500')
+    elsif type == 'main'
+      image_generator(height: '400', width: '200')
+    end
+  end
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,8 +3,8 @@ class Portfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda {|attrs| attrs['name'].blank?}
 
-    include Placeholder
-    validates_presence_of :title , :body, :main_image, :thumb_image
+    
+    validates_presence_of :title , :body
     
     mount_uploader :thumb_image, PortfolioUploader
     mount_uploader :main_image, PortfolioUploader
@@ -18,10 +18,7 @@ class Portfolio < ApplicationRecord
     end
     scope  :service, -> { where(subtitle: 'My great service')}
     
-    after_initialize :set_defaults
     
-    def set_defaults
-        self.main_image ||=  Placeholder.image_generator(height: '400', width: '200')
-        self.thumb_image ||=  Placeholder.image_generator(height: '300', width: '500')
-    end
+    
+    
 end

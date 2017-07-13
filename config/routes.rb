1c54952e75fb92aa576cb179d196145cf5c2b97b
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-13 16:09:51 +0000

    Rendered tweets on their own page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   get 'contact', to: 'pages#contact'
   
+  get 'tech-news', to: 'pages#tech_news'
+  
   get 'portfolio/:id', to: 'portfolios#show' , as: 'portfolio_show'
 
   

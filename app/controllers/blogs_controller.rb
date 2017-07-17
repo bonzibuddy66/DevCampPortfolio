bd96f04ef0218d9e24aa7b289ac9b4f0d5b2e9e5
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-17 17:22:58 +0000

    Added a topic sidebar

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,7 +8,7 @@ class BlogsController < ApplicationController
     if logged_in? (:site_admin)
     @blogs = Blog.recent.page(params[:page]).per(5)
   else
-    @blogs = Blog.published..recent.page(params[:page]).per(5)
+    @blogs = Blog.published.recent.page(params[:page]).per(5)
   end
     @page_title = "My Portfolio Blog"
   end
@@ -16,10 +16,14 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-    @blog = Blog.includes(:comments).friendly.find(params[:id])
-    @comment = Comment.new
-    @page_title = @blog.title
-    @seo_keywords = @blog.body
+    if logged_in?(:site_admin) || @blog.published?
+      @blog = Blog.includes(:comments).friendly.find(params[:id])
+      @comment = Comment.new
+      @page_title = @blog.title
+      @seo_keywords = @blog.body
+    else
+      redirect_to blogs_path, notice: "You are not authorized to access this page!"
+    end
   end
 
   # GET /blogs/new
@@ -86,7 +90,7 @@ def toggle_status
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :body)
+      params.require(:blog).permit(:title, :body, :topic_id)
     end
   end
 
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,9 +2,9 @@ class Blog < ApplicationRecord
     enum status: {draft: 0 , published: 1}
     extend FriendlyId
     friendly_id :title, use: :slugged
-    validates_presence_of :title, :body
+    validates_presence_of :title, :body, :topic_id
     # Comment "belongs_to :topic" out if you're having bugs!
-    #belongs_to :topic
+    belongs_to :topic
     
     has_many :comments, dependent: :destroy
     

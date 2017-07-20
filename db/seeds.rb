6b8cea67d068167b6cd0a1fdfd8c30c798fa2aea
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-20 15:09:00 +0000

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+        email: "test3@test.com",
+        password: "abcd12345",
+        password_confirmation: "abcd12345",
+        name: "Admin User",
+        roles: "site_admin"
+    )
+puts "1 Admin User Created"
+
+User.create!(
+        email: "test2@test.com",
+        password: "abcd12345",
+        password_confirmation: "abcd12345",
+        name: "John Doe",
+    )
+puts "1 User Created"
 
 
 3.times do |topic|
